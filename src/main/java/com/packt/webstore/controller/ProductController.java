2089dd170263440b48d5208c6f5ada61e75b1c1f
Admin-Komputer\Admin <ehe241@gmail.com>
2018-03-23 14:37:23 +0100

    Deleted useless imports.

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.packt.webstore.controller;
 
 
-import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.math.BigDecimal;
 
 @Controller
 public class ProductController {
