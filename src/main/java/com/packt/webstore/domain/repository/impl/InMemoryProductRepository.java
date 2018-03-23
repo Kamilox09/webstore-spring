3df5e2a5d5f13802b727d55ed0507eb389381e40
Admin-Komputer\Admin <ehe241@gmail.com>
2018-03-23 16:19:25 +0100

    Added the order service.

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -43,4 +43,20 @@ public class InMemoryProductRepository implements ProductRepository {
     public List<Product> getAllProducts() {
         return listOfProducts;
     }
+
+    public Product getProductById(String productId) {
+        Product productById = null;
+        for (Product product : listOfProducts) {
+            if (product != null &&
+                    product.getProductId() != null &&
+                    product.getProductId().equals(productId)) {
+                productById = product;
+                break;
+            }
+        }
+        if (productById == null) {
+            throw new IllegalArgumentException("Brak produktu i wskazanym id: " + productId);
+        }
+        return productById;
+    }
 }
